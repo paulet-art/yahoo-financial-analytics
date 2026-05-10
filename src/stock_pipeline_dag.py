3db4841d56329581aa8dbe251055237d4afffb08
paulet-art <wanjirupaulet@gmai.com>
2026-05-10 20:16:16 +0300

    Refactor data ingestion process and add Airflow DAG for stock data pipeline

---
+++ src/stock_pipeline_dag.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+import sys
+import os
+
+sys.path.append('/opt/airflow/src')
+
+from ingest_data import run
+
+default_args = {
+    "owner": "paulet",
+    "retries": 2,
+    "retry_delay": timedelta(minutes=1)
+}
+
+def run_stock_pipeline():
+    symbols = ["NVDA"]
+    run(symbols)
+
+with DAG(
+    dag_id="stock_data_pipeline",
+    default_args=default_args,
+    description="Financial stock ingestion pipeline",
+    start_date=datetime(2025, 1, 1),
+    schedule_interval="@daily",
+    catchup=False
+) as dag:
+
+    ingest_task = PythonOperator(
+        task_id="run_ingestion",
+        python_callable=run_stock_pipeline
+    )
+
+    ingest_task
